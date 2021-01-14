68228179d7c0e0beea1ae08bd6c8cc58f938a549
Wadzek1 <frederik.molkenbur@gmail.com>
2021-01-14 23:31:31 +0100

    Adding newlines for better overview between cmds

--- main.py
+++ main.py
@@ -48,23 +48,30 @@ async def on_message(message):
 
   if msg_text.startswith('!hello'):
     await message.channel.send(HELLO_WORLD_STRING)
+
   elif msg_text.startswith("!inspiration"):
     quote = get_quote()
     await message.channel.send(quote)
+
   elif msg_text.startswith("!poll"):
     await message.add_reaction("1️⃣")
     await message.add_reaction("2️⃣")
+
   elif any(word in msg_text for word in BAD_WORDS):
     await message.channel.send(MESSAGES["wortwahl"])
+
   elif msg_text.startswith("!machterfehler?"):
     await message.channel.send("Ich mache keine Fehler, " + message.author.mention)
+
   elif msg_text.startswith("!quit"):
+
     if message.author.id in [323196084413267974, 277159146565009408]:
       await message.channel.send(f"Tschüß, {message.author.mention}!")
       await client.close()
       exit(0)
     else:
       await message.channel.send(f"Das darfst du nicht, {message.author.mention}!")
+
   elif msg_text.startswith("!info"):
     await message.channel.send("Hier sind Informationen über mich, du minderwertiges Stück Kohlenstoff:")
     await message.channel.send("https://wiki.coldmirror.net/wiki/A.R.S.C.H_9000")
@@ -82,7 +89,7 @@ async def on_message(message):
     await message.channel.send(f"""Top Hit on Twitch:
 ```
 {top_hit}
-```""") 
+```""")
 
   elif msg_text.startswith("!istkirilive?"):
     oauth = os.getenv("TWITCH_OAUTH_TOKEN")
@@ -94,21 +101,23 @@ async def on_message(message):
 
     request = requests.get("https://api.twitch.tv/helix/search/channels", params=parameters, headers=headers)
     top_hit = request.json()["data"][0]
-    
+
     if not top_hit["is_live"]:
       await message.channel.send("kiron ist nicht live")
       await message.channel.send("Wenn Kiron streamt, kannst du ihn hier finden:")
       await message.channel.send("https://twitch.tv/kirimctwitch")
     else:
       await message.channel.send("kiron ist live → https://kirimcplay.tv/twitch")
+
   elif msg_text.startswith("!wannwarderersteweltkrieg?"):
     await message.channel.send("Der erste Weltkrieg ging vom 28. Juli 1914 bis zum 11. November 1918")
+
   elif msg_text.startswith("!wissen") and (len(msg_text.split(" ")) == 2):
     try:
       search_term = msg_text.split(" ")[1]
       summary = wikipedia.summary(search_term)
       summary_parts = summary.split(". ")
-      
+
       if len(summary_parts) >= 3:
         summary = f"{summary_parts[0]}.   {summary_parts[1]}. {summary_parts[2]}."
       elif len(summary_parts) == 2:
@@ -117,7 +126,7 @@ async def on_message(message):
         summary = f"{summary_parts[0]}."
       else:
         message.channel.send("Tut mir leid, {message.author.mention}, dazu konnte ich keine Zusammenfassung finden")
-  
+
       if len(summary) > 2000:
         link = f"https://lmgtfy.app/#gsc.tab=0&gsc.q={search_term}"
         await message.channel.send(f"Entschuldige, {message.author.mention}, die ERSTEN DREI SÄTZE der Wikipedia-Zusammenfassung ist schon zu lang für eine Discord-Nachricht.\n{link}")
